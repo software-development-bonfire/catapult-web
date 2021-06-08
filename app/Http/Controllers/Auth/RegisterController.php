9b2987d89017b73ea702ae9d4f7ddb88ccd49797
keyven14 <keyvenrosal.bonfire@gmail.com>
2021-06-08 10:14:18 +0800

    update .env.example, added database config

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,8 +49,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'full_name' => ['required', 'string', 'max:45'],
+            'username' => ['required', 'string', 'max:45'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -64,8 +64,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'full_name' => $data['name'],
+            'username' => $data['name'],
             'password' => Hash::make($data['password']),
         ]);
     }
