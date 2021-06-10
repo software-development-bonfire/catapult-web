282937a5a8316ce8943ef5ac859bb48724b91705
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2021-06-10 10:11:57 +0800

    :star: FT_35: Sample integration - Added sample label language variable

--- resources/js/vue-i18n-locales.generated.js
+++ resources/js/vue-i18n-locales.generated.js
@@ -4,7 +4,9 @@ export default {
             "failed": "These credentials do not match our records.",
             "throttle": "Too many login attempts. Please try again in {seconds} seconds."
         },
-        "label": [],
+        "label": {
+            "login": "Login"
+        },
         "pagination": {
             "previous": "&laquo; Previous",
             "next": "Next &raquo;"
