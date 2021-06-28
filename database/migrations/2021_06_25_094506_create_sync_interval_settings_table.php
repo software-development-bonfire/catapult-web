4124ace32dfde79e2b713bd419d10f4225fec751
keyven14 <keyvenrosal.bonfire@gmail.com>
2021-06-28 16:47:22 +0800

    :fire: FT_34: Change Tab to spaces

--- database/migrations/2021_06_25_094506_create_sync_interval_settings_table.php
+++ database/migrations/2021_06_25_094506_create_sync_interval_settings_table.php
@@ -16,7 +16,7 @@ class CreateSyncIntervalSettingsTable extends Migration
 	 */
 	public function up()
 	{
-		Schema::create('sync_interval_settings', function(Blueprint $table) {
+        Schema::create('sync_interval_settings', function(Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('bid')->index()->unique();
             $table->string('name', 45);
@@ -24,11 +24,11 @@ class CreateSyncIntervalSettingsTable extends Migration
             $table->string('syncing_type', 45);
             $table->string('start_time')->nullable();
             $table->tinyInteger('status');
-			$table->unsignedBigInteger('created_by');
-			$table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-			$table->softDeletes();
+            $table->softDeletes();
 		});
 	}
 
