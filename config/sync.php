e7e9de609a993562b55520b350189e5fe79cdc56
marianz-bonfire <mariano.degracia.bonfire@gmail.com>
2022-12-07 10:19:31 +0800

    :bug: FT_77: Trigger Re-fetching - Related to CDIS Modification - Finalized default values in sync configuration
    - Added definition of each configuration

--- config/sync.php
+++ config/sync.php
@@ -1,18 +1,24 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Default Command parameters in Catapult
+|--------------------------------------------------------------------------
+| These parameters will automatically the value of console command
+| parameters if there are no payload data options provided in CDIS events
+|
+| limit             : This will be the limit to be fetched and synced to Catapult, means every limit will be fetched
+|                     and synced then deleted in [sync] table (CDIS) and then proceed to another chunk of bids in [sync] table.
+| convert_limit     : This will be the maximum limit value to be converted, used in conversion process.
+| broadcast         : Set to True if you want to broadcast the process. Helps to show activity message in Developer Options.
+| show_progress     : Set to True if you want to show progress in Catapult process such syncing and conversion.
+|                     Note: this will be ignored when options.broadcast was set to False
+| progress_divisor  : If options.show_progress is False, then it will automatically applied to show progress
+|                     every progress_divisor. It helps to control usage of Pusher message request
+*/
+
 return [
     'restart' => false,
-
-     /*
-    |--------------------------------------------------------------------------
-    | Default Command parameters in CDIS to POS
-    |--------------------------------------------------------------------------
-    | These parameters will automatically the value of console command
-    | parameters if there are no payload data options provided in CDIS events
-    |
-    | 'limit' is the number of bids in a chunks to be synced
-    |
-    */
     'cdis' => [
         'to_catapult' => [
             'limit' => 300,
