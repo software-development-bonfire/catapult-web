278eb38d74157ee84870fe443c0875c805ca536c
marianz-bonfire <mariano.degracia.bonfire@gmail.com>
2023-02-23 17:49:25 +0800

    :fire: HF_85: Terminal File Setup - Fix to Avoid Moving POS' file to other path - Changed version from `'2.1.0.1` to `'2.1.0.2`

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 use App\Providers\RepositoryServiceProvider;
 
 return [
-    'version' => '2.1.0.1',
+    'version' => '2.1.0.2',
 
     /*
     |--------------------------------------------------------------------------
