0af47a896bafa7616e21b7988173c0e5414e2cff
marianz-bonfire <mariano.degracia.bonfire@gmail.com>
2023-06-05 10:53:10 +0800

    :fire: HF_87: Branch & Terminal Sync from CDIS - Resolved code review comments

--- app/Console/Commands/Tools/NetworkTroubleshooter.php
+++ app/Console/Commands/Tools/NetworkTroubleshooter.php
@@ -52,7 +52,7 @@ class NetworkTroubleshooter extends Command
 
         $this->info(__('info.setting_dns_for_all_connected_interface'));
         $errors = [];
-        foreach ($networkResolver->setDnsConnectedInterface(false) as $interfaceName){
+        foreach ($networkResolver->setDnsConnectedInterface(false) as $interfaceName) {
             $result = $networkResolver->setDns($interfaceName);
             if ($result && ! is_array($result)) {
                 foreach ($networkResolver->showSetDns($interfaceName) as $output) {
