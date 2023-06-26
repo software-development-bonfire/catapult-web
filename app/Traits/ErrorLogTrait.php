30876bffc85765cee55877ec34e0aecabf3eb998
marianz-bonfire <mariano.degracia.bonfire@gmail.com>
2023-06-26 11:23:52 +0800

    :fire: HF_88: Cost and Price Change CSV | Modification and Catapult Scheduler - Removed some commented lines due to testing

--- app/Traits/ErrorLogTrait.php
+++ app/Traits/ErrorLogTrait.php
@@ -16,7 +16,6 @@ use App\Traits\FilenameRetryCounterTrait;
 trait ErrorLogTrait
 {
     use FilenameRetryCounterTrait;
-    //use ConsoleCommandTrait;
 
     /**
      * Insert errors into database
@@ -72,7 +71,6 @@ trait ErrorLogTrait
                     $details = $errorLog->details;
                     $spaces = ($entriesMaxLength - strlen($errorLog->filename)) / 2;
                     $filenameLog = str_repeat(' ', ceil($spaces)) . $errorLog->filename . str_repeat(' ', floor($spaces));
-                    //$filenameLog = $this->computedLogLabel($entriesMaxLength, $errorLog->filename);
 
                     if ($details !== null) {
                         foreach ($details as $detail) {
