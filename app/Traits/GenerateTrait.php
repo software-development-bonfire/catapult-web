35aa04784faa7f8d2f18ea3155a7ec560fc41b0d
marianz-bonfire <mariano.degracia.bonfire@gmail.com>
2023-06-26 17:39:17 +0800

    :fire: HF_89: Catapult Syncing Optimization - Fixed jobs queueing and logic affected by cost and price change scheduling
    - Created enum `DefinedQueueName` for collection of queue names
    - Added function in generation of CSV for flagging `Create CSV for New Branch`
    - Added new sync configuration element

--- app/Traits/GenerateTrait.php
+++ app/Traits/GenerateTrait.php
@@ -4,12 +4,13 @@ namespace App\Traits;
 
 use App\Enums\DisplayState;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 
 trait GenerateTrait
 {
     public function setGenerated($model, $bids, $isGenerated = DisplayState::YES)
-    {        
+    {
         $data = [
             'is_generated' => $isGenerated,
             'generated_at' => Carbon::now(),
@@ -18,5 +19,15 @@ trait GenerateTrait
         if ($model) {
             $model->update($data);
         }
+    }
+
+    public function setBranchGenerated()
+    {
+        Storage::disk('public')->put('CSV', 'This file means Create CSV for New Branch already executed.');
+    }
+
+    public function isBranchGenerated()
+    {
+        return (Storage::disk('public')->exists('CSV'));
     }
 }
