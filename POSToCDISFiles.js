030c11223aa7a994713de66e853b254c92c017a9
coybonfire <coy.bonfire@gmail.com>
2025-01-09 09:18:00 +0800

    :star: HF_0000205: POS To CDISFiles Data Sync

---
+++ POSToCDISFiles.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const env = require('dotenv');
+const chokidar = require('chokidar');
+
+env.config({ path: ".env" });
+
+const destinationParentDirectory = process.env.CDIS_FILES_LOCAL_PATH;
+const sourceParentDirectory = process.env.POS_FILES_LOCAL_PATH;
+
+const ChildDirectory = process.env.POS_DIRECTORIES.split(",");
+
+const option = {
+    ignoreInitial: true,
+}
+    
+ChildDirectory.forEach(item => {
+    chokidar.watch(sourceParentDirectory + item, option)
+        .on('add', path => copyFile(path, item))
+});
+
+function copyFile(srcFilePath, folderName) {
+    
+    let srcPath = srcFilePath.replace(/\\/g, '/');
+    let fileName = srcPath.replace(sourceParentDirectory+folderName, '');
+   
+    fs.cp(srcPath, destinationParentDirectory + folderName + fileName, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+}
