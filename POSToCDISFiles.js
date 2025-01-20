488a56ac26e151581ffb14a752a3d136bc530dee
coybonfire <coy.bonfire@gmail.com>
2025-01-20 15:04:33 +0800

    :fire: HF_0000205: POS to CDISFiles Data Fetch

--- POSToCDISFiles.js
+++ POSToCDISFiles.js
@@ -7,7 +7,7 @@ env.config({ path: ".env" });
 const destinationParentDirectory = process.env.CDIS_FILES_LOCAL_PATH;
 const sourceParentDirectory = process.env.POS_FILES_LOCAL_PATH;
 
-const ChildDirectory = process.env.POS_DIRECTORIES.split(",");
+const childDirectory = process.env.POS_DIRECTORIES.split(",");
 const daysDelay = 7;
 const option = {
     ignoreInitial: false,
@@ -19,7 +19,7 @@ let transfered = JSON.parse(fs.readFileSync(fileNames).toString());
 let givenDate = new Date();
     givenDate = givenDate.setDate(givenDate.getDate() - daysDelay);
     
-ChildDirectory.forEach(item => {
+childDirectory.forEach(item => {
     chokidar.watch(sourceParentDirectory + item, option)
         .on('add', path => copyFile(path, item))
         .on('change', path => copyFile(path, item))
