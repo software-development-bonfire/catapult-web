5fe870f411388154f7840a67fa7d378e387f0689
coybonfire <coy.bonfire@gmail.com>
2026-09-11 17:12:42 +0800

    :bug: HF_0000034: KDS - Release Requirements

--- app/Enums/KDS/KDSActionType.php
+++ app/Enums/KDS/KDSActionType.php
@@ -7,9 +7,9 @@ use BenSampo\Enum\Enum;
 final class KDSActionType extends Enum
 {
     const FOR_PREPARE = 1;
-    const FOR_SERVE  = 2;
-    const FOR_BUMP  = 3;
-    const FOR_ASSEMBLY  = 4;
+    const FOR_BUMP  = 2;
+    const FOR_ASSEMBLY  = 3;
+    const FOR_SERVE  = 4;
     const FOR_RECALL  = 5;
     const FOR_DONE  = 6;
 
